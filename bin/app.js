6201e096185f52b6371df237f478dad34b704db0
mzaini30 <muhzaini30@gmail.com>
2018-01-29 18:03:19 +0000

    oke

--- bin/app.js
+++ bin/app.js
@@ -1,6 +1,11 @@
 jQuery(function(){
 	$('.convert').click(function(){
-		nomor_whatsapp = $('#nomor-whatsapp').val();
-		$('#hasil').val('https://api.whatsapp.com/send?phone='+nomor_whatsapp+'&text='+isi+'');
+		if ($('[required]').val() == ''){
+			alert('Nomor Whatsapp harus diisi.');
+		} else {
+			nomor_whatsapp = $('#nomor-whatsapp').val();
+			isi = $('#isi-pesan').val();
+			$('#hasil').val('https://api.whatsapp.com/send?phone='+nomor_whatsapp+'&text='+isi+'');
+		}
 	});
 });
