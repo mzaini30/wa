2560fa18eda366b0fb101e262ff88d7f38f1e37b
mzaini30 <muhzaini30@gmail.com>
2018-01-29 18:12:02 +0000

    oke

--- bin/app.js
+++ bin/app.js
@@ -6,7 +6,9 @@ jQuery(function(){
 			nomor_whatsapp = $('#nomor-whatsapp').val();
 			isi = $('#isi-pesan').val();
 			isi = isi.replace(/ /g, '%20');
-			$('#hasil').val('https://api.whatsapp.com/send?phone='+nomor_whatsapp+'&text='+isi+'');
+			isi = isi.replace(/:/g, '%3A');
+			isi = isi.replace(/\n/g, '%0A');
+			$('#hasil').val('https://api.whatsapp.com/send?phone='+nomor_whatsapp+'&text='+isi);
 		}
 	});
 });
